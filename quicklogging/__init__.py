bfefca5b5d5d04c4cfe4005b0d67355dccc44ccb
Feth Arezki <feth@majerti.fr>
2016-05-18 01:43:06 +0200

    Add catch prints fn and much doc

--- quicklogging/__init__.py
+++ quicklogging/__init__.py
@@ -1,98 +1,167 @@
 """
-logging wrapper
+Module quicklogging
+=======================
 
-Here lie only a few wrappers/tools, including those used by the app.ini config
+(logging with a bit of cozyness)
 
-Supplied convenience functions fetch a logger with the name of the module from
-which you're calling them.
+log wrappers
+-------------
+
+    Supplied convenience functions fetch a logger with the name
+    of the module from which you're calling them.
+
+.. autofunction:: debug
+
+.. autofunction:: info
+
+.. autofunction:: warning
+
+.. autofunction:: error
+
+.. autofunction:: critical
+
+.. autofunction:: exception
+
+print handlers
+---------------
+
+.. autofunction:: catch_prints
+
+.. autofunction:: warn_prints
+
 """
 
-import inspect
-import logging
+import sys
 
-
-def get_logger(stackoverhead=0):
-    """
-    wrapper for getLogger
-
-    Typical use, imaging you're in project/module/submodule.py:
-
-    >>> l = get_logger()
-    >>> l
-    <logging.Logger at ... >
-    >>> l.name
-    project.module.submodule
-
-    :param stackoverhead: defaults to 1. How deep to look in the stack for
-        fetching the logger name.
-
-    :rtype: logging.Logger
-
-    .. note::
-        this function may be expanded
-    """
-    frm = inspect.stack()[1 + stackoverhead]
-    mod = inspect.getmodule(frm[0])
-
-    if mod is None:
-        basename = "<No module>"
-    else:
-        basename = mod.__name__
-
-    return logging.getLogger(basename)
-
-
-def _log_with_level(func_name, *args, **kwargs):
-    stackoverhead = kwargs.pop('stackoverhead', 0)
-    logger = get_logger(stackoverhead=stackoverhead + 1)
-    logfunc = getattr(logger, func_name)
-    logfunc(*args, **kwargs)
+from .base import _log_with_level
+from .base import get_logger
+from .stream_wrapper import StreamWrapper
 
 
 def debug(*args, **kwargs):
-    _log_with_level('debug', stackoverhead=1, *args, **kwargs)
+    """wrapper for :py:meth:`logging.Logger.debug`
+
+    Variadic parameters: see :py:meth:`logging.Logger.debug`
+
+    :param int stackoverhead: see :py:func:`quicklogging.base.get_logger`
+    """
+    stackoverhead = kwargs.pop('stackoverhead', 1)
+    _log_with_level('debug', stackoverhead=stackoverhead, *args, **kwargs)
 
 
 def info(*args, **kwargs):
-    _log_with_level('info', stackoverhead=1, *args, **kwargs)
+    """wrapper for :py:meth:`logging.Logger.info`
+
+    Variadic parameters: see :py:meth:`logging.Logger.info`
+
+    :param int stackoverhead: see :py:func:`quicklogging.base.get_logger`
+    """
+    stackoverhead = kwargs.pop('stackoverhead', 1)
+    _log_with_level('info', stackoverhead=stackoverhead, *args, **kwargs)
 
 
 def warning(*args, **kwargs):
-    _log_with_level('warning', stackoverhead=1, *args, **kwargs)
+    """wrapper for :py:meth:`logging.Logger.warning`
+
+    Variadic parameters: see :py:meth:`logging.Logger.warning`
+
+    :param int stackoverhead: see :py:func:`quicklogging.base.get_logger`
+    """
+    stackoverhead = kwargs.pop('stackoverhead', 1)
+    _log_with_level('warning', stackoverhead=stackoverhead, *args, **kwargs)
 
 
 def error(*args, **kwargs):
-    _log_with_level('error', stackoverhead=1, *args, **kwargs)
+    """wrapper for :py:meth:`logging.Logger.error`
+
+    Variadic parameters: see :py:meth:`logging.Logger.error`
+
+    :param int stackoverhead: see :py:func:`quicklogging.base.get_logger`
+    """
+    stackoverhead = kwargs.pop('stackoverhead', 1)
+    _log_with_level('error', stackoverhead=stackoverhead, *args, **kwargs)
 
 
 def critical(*args, **kwargs):
-    _log_with_level('critical', stackoverhead=1, *args, **kwargs)
+    """wrapper for :py:meth:`logging.Logger.critical`
+
+    Variadic parameters: see :py:meth:`logging.Logger.critical`
+
+    :param int stackoverhead: see :py:func:`quicklogging.base.get_logger`
+    """
+    stackoverhead = kwargs.pop('stackoverhead', 1)
+    _log_with_level('critical', stackoverhead=stackoverhead, *args, **kwargs)
 
 
 def exception(*args, **kwargs):
-    _log_with_level('exception', stackoverhead=1, *args, **kwargs)
+    """wrapper for :py:meth:`logging.Logger.exception`
+
+    Variadic parameters: see :py:meth:`logging.Logger.exception`
+
+    :param int stackoverhead: see :py:func:`quicklogging.base.get_logger`
+    """
+    stackoverhead = kwargs.pop('stackoverhead', 1)
+    _log_with_level('exception', stackoverhead=stackoverhead, *args, **kwargs)
 
 
-_CUR_LOGGER_CHILDREN = object()
+_CUR_MODULE = object()
+
+
+def warn_prints(catch_all=False):
+    """Activate warning when print is called
+
+    :param bool catch_all: defaults to False, ie. defaults to only warn about
+        current module, ignoring imports"""
+    raise NotImplementedError("API is yet to be defined, please help me.")
+
+
 def catch_prints(
-    catch_all=False
-    include_children=_CUR_LOGGER_CHILDREN,
-    exclude_children=(),
-    warn='never'
-    ):
-    """trigger print() catching: redirects calls to a logger
+    catch_module=_CUR_MODULE,
+    catch_all=False,
+    include_children=True,
+    logfunc=info,
+):
+    """configure the print() catching: redirects calls to a logger
 
     By default, only catches calls to print() from logger
 
     * named after the calling module
     * children of this logger
 
-    this is not implemented yet: API should be discussed a bit
 
+    .. note::
+
+        API discussion welcome.
+
+        You anderstand the API is not stable.
+
+    :param string catch_module: include children of logger designed by name
     :param bool catch_all: should catch all print() diregarding where
         they're from ?
-    :param warn: one of "always", "once", "never"
-    :param string include_children: include children of logger designed by name
-    :param iterable exclude_children
+
+        .. warning::
+
+            take care of logging propagation (``Logger.propagate()̀``)
+
+    :param function logfunc: function to use for logging messages
+
+    Possible extension ideas:
+
+        * wrap arbitrary output stream
+        * different log functions depending on regex applied on messages
+        * make it configurable from config file
+        * allow exclusion of specific modules
     """
-    raise NotImplementedError("Not yet, wait a bit!")
+    wrapper, created = StreamWrapper.get_wrapper(
+        'sys.stdout',
+        sys.stdout,
+        logfunc,
+        catch_all=catch_all,
+    )
+    if created:
+        sys.stdout = wrapper
+
+    if catch_module is _CUR_MODULE:
+        catch_module = get_logger(stackoverhead=1).name
+
+    wrapper.include_in_catch(catch_module, include_children)
