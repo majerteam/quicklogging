abafc5c59915c4ee352886f70aa2f313a7449949
Feth AREZKI <feth@majerti.fr>
2022-05-02 11:34:51 +0200

    bump intersphinx mapping to use Py 3.9 doc

--- conf.py
+++ conf.py
@@ -297,4 +297,4 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/3.5/': None}
+intersphinx_mapping = {'https://docs.python.org/3.9/': None}
