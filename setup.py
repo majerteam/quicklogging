f25a5ad47ffca26c685e622527eeedd86f9b7c32
Feth AREZKI <feth@majerti.fr>
2022-05-02 15:38:46 +0200

    revamp packaging

--- setup.py
+++ setup.py
@@ -1,21 +1,6 @@
+#!/usr/bin/env python
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+import setuptools
 
-config = {
-    'description': 'Logging wrapper',
-    'author': 'Feth Arezki',
-    'url': 'https://github.com/majerteam/quicklogging',
-    'download_url': 'https://github.com/majerteam/quicklogging',
-    'author_email': 'tech@majerti.fr',
-    'version': '0.3',
-    'packages': ['quicklogging'],
-    'setup_requires': [],
-    'tests_require': ['pytest-runner', 'pytest', 'pytest-cov', 'six', 'stringimporter>=0.1.3'],
-    'name': 'quicklogging',
-    'zip_safe': False,
-}
-
-setup(**config)
+if __name__ == "__main__":
+    setuptools.setup()
